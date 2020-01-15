4731999f5c9a4a9b6dae1e6bf1834f8605506004
Roman Popper <romanpopper@gmail.com>
2020-01-15 20:43:18 +0100

    Add project files.

---
+++ eRkoSongBook/eRkoSongBook.Android/SplashActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace eRkoSongBook.Droid
+{
+    [Activity(Theme = "@style/SplashTheme", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupTask = new Task(() => { SplashTheScreen(); });
+            startupTask.Start();
+        }
+
+        public override void OnBackPressed()
+        {
+        }
+
+        private async void SplashTheScreen()
+        {
+            await Task.Delay(1000);
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+
+    }
+}
